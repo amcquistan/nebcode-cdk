6fcd9059898e94519d32c696ec1774273daa2643
Adam McQuistan <sci.guy.mcq@gmail.com>
2022-06-30 09:19:33 -0500

    Initial commit

---
+++ test/nebcode-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NebcodeCdk from '../lib/nebcode-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/nebcode-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NebcodeCdk.NebcodeCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
